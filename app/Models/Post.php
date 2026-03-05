82e02b4278090bc066d73c291ce4a46af26a7667
Lukianov Illia <Lukjanovilly@gmail.com>
2026-03-05 10:13:50 +0200

    Add Eloquent relationships to blog models

---
+++ app/Models/Post.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Post extends Model
+{
+    protected $fillable = ['title', 'slug', 'content', 'excerpt', 'user_id', 'category_id', 'published_at'];
+    protected $casts = ['published_at' => 'datetime'];
+
+    /**
+     * Get the author of this post
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the category of this post
+     */
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class)->withDefault();
+    }
+
+    /**
+     * Get all tags for this post
+     */
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class, 'post_tags');
+    }
+}
