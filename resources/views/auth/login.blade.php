e3be46ff6c2fd6eaa0b824b8b39ec1d65a4ade3a
Lukianov Illia <Lukjanovilly@gmail.com>
2026-03-05 08:18:53 +0200

    Remove autofill from login form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,18 +12,18 @@
         </div>
     @endif
 
-    <form method="POST" action="{{ route('login') }}">
+    <form method="POST" action="{{ route('login') }}" autocomplete="off">
         @csrf
 
         <div class="form-group">
             <label for="email" class="form-label">Email Address</label>
-            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email', 'test@example.com') }}" placeholder="test@example.com" required autofocus />
+            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" placeholder="test@example.com" required autofocus autocomplete="off" />
             @error('email')<div class="invalid-feedback">{{ $message }}</div>@enderror
         </div>
 
         <div class="form-group">
             <label for="password" class="form-label">Password</label>
-            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" value="password123" placeholder="Enter your password" required />
+            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" placeholder="Enter your password" required autocomplete="off" />
             @error('password')<div class="invalid-feedback">{{ $message }}</div>@enderror
         </div>
 
